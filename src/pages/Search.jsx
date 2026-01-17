a7575c60592faeadbf3755eac25eaa7f1a29db2e
dyegomelo20 <dyegomelo16@gmail.com>
2026-01-17 19:13:49 -0300

    feat(search): adiciona estilos e ajusta renderização dos dados

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -3,6 +3,7 @@ import { useSearchApi } from "../hooks/useApi"
 import estrela from "../assets/estrela.png"
 import { useEffect, useState } from "react";
 import CustomButton from "../components/global/CustomButton";
+import "./Search.css"
 
 const Search = () => {
     const [searchParams] = useSearchParams();
@@ -18,18 +19,21 @@ const Search = () => {
 
     if (loading) return <p>Carregando...</p> 
   return (
-    <div>
+    <div className="search">
         <h1>Reasultados: {query}</h1>
-        <div>
+        <div >
             <ul>
-                {data.results.map((multi) => (
+                {data.results.filter((multi) => multi.media_type !== "person").map((multi) => (
                     <li key={multi.id}>
-                        {multi.poster_path ? <img src={`https://image.tmdb.org/t/p/w500/${multi.poster_path}`} alt="" /> : <div><p>Sem imagem</p></div>}
+                        {multi.poster_path ? <img src={`https://image.tmdb.org/t/p/w500/${multi.poster_path}`} alt="" /> : <div className="null-img"><p>Sem imagem</p></div>}
                         <h2>{multi.media_type === "movie" ? multi.title : multi.name}</h2>
                         <div className="classificacao">
-                        <p>{multi.media_type === "movie" ? multi.release_date.slice(0, 4) : multi.first_air_date}</p>
+                        <p>{multi.media_type === "movie" ? multi.release_date.slice(0, 4) : multi.first_air_date.slice(0, 4)}</p>
+                        {multi.vote_average > 0 &&( 
+                        <>
                         <img src={estrela} alt="Estrela" className="estrela"/>
                         <p>{Math.floor(multi.vote_average * 10) / 10}</p>
+                        </>)}
                         </div>
                     </li>
                 ))}
