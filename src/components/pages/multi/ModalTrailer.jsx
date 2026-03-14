af74d989249581d2b34f8906ec6821f739a700f0
dyegomelo20 <dyegomelo16@gmail.com>
2026-03-13 21:34:14 -0300

    feat: Exibe mensagem de erro dentro do modal quando trailer não está disponível

--- src/components/pages/multi/ModalTrailer.jsx
+++ src/components/pages/multi/ModalTrailer.jsx
@@ -12,22 +12,27 @@ const ModalTrailer = ({id, type, ativo, onClose}) => {
     useEffect(() => {
     if (data?.results?.length > 0) {
       setTrailer(data.results[counte])
-    }else if (data && data.results?.length === 0) {
-      alert("Trailer não disponível")
-      onClose()
     }
     }, [data, counte])
 
-    if (!trailer) return null
+    const hasNoTrailer = data && data.results?.length === 0;
+
+    if (!trailer && !hasNoTrailer) return null
 
   return (
     <div className='overlay' onClick={onClose}>
         <div className={`modal ${ativo}`} onClick={(e) => e.stopPropagation()}>
-            <iframe  src={`https://www.youtube.com/embed/${trailer.key}`} frameBorder="0" ></iframe>
-            <div className="btn-trailer">
-              <CustomButton onClick={() => setCounte(counte - 1)} disabled={counte === 0}>Anterior</CustomButton>
-              <CustomButton onClick={() => setCounte(counte + 1)} disabled={counte === data?.results?.length - 1}>Próximo</CustomButton>
-            </div>
+            {hasNoTrailer ? (
+              <h2 style={{ color: "#fff", textAlign: "center" }}>Trailer indisponível</h2>
+            ) : (
+              <>
+                <iframe  src={`https://www.youtube.com/embed/${trailer.key}`} frameBorder="0" ></iframe>
+                <div className="btn-trailer">
+                  <CustomButton onClick={() => setCounte(counte - 1)} disabled={counte === 0}>Anterior</CustomButton>
+                  <CustomButton onClick={() => setCounte(counte + 1)} disabled={counte === data?.results?.length - 1}>Próximo</CustomButton>
+                </div>
+              </>
+            )}
         </div>
     </div>
   )
